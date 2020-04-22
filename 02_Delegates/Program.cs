222ddea67d9dd0b6768f9fdc621c237ee996223c
Joao Pedro <jp.ssilva@live.com>
2020-04-22 16:04:36 -0300

    Introducao básica Delegate 03

--- 02_Delegates/Program.cs
+++ 02_Delegates/Program.cs
@@ -12,11 +12,27 @@ namespace _02_Delegates
         static void Main(string[] args)
         {
 
-            Foto foto = new Foto { Nome = "foto.jpg", TamanhoX = 1920, TamanhoY = 1000 };
+            //Passando um objeto como parâmetro
 
-            FotoProcessador.Processador(foto);
+            //Simulações
+            //Tela de cadastro de usuário - Thumb (foto do perfil)
+            Foto fotoUsuario = new Foto { Nome = "perfil.jpg", TamanhoX = 1920, TamanhoY = 1000 };
+            FotoProcessador.filtros = new FotoFiltro().GerarThumb;
+            FotoProcessador.Processar(fotoUsuario);//Passando um objeto como parâmetro
 
-            Console.WriteLine(foto);
+            //Tela de cadastro de produtos: Colorir + Tamanho Médio
+            Foto fotoProduto = new Foto { Nome = "produto.jpg", TamanhoX = 1920, TamanhoY = 1000 };
+            FotoProcessador.filtros = new FotoFiltro().Colorir;
+            //Para haver a outra chamada de um método delegate deve-se usar o += para concatenar
+            FotoProcessador.filtros += new FotoFiltro().RedimensionarTamMedio;
+            FotoProcessador.Processar(fotoProduto);
+
+
+            //Tela de cadastro de Álbum do Usuário - Preto e Branco
+            Foto fotoAlbum = new Foto { Nome = "album.jpg", TamanhoX = 1920, TamanhoY = 1000 };
+            FotoProcessador.filtros = new FotoFiltro().PretoBranco;
+            FotoProcessador.Processar(fotoAlbum);
+
 
             Console.ReadLine();
 
