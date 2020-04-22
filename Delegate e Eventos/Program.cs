5d2170d35f3b31c9110156df0ec7f7e9feac9d1b
Joao Pedro <jp.ssilva@live.com>
2020-04-22 11:37:32 -0300

    Introducao básica Delegate 01

--- Delegate e Eventos/Program.cs
+++ Delegate e Eventos/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Delegate_e_Eventos
+namespace _01_Delegates
+
 {
     class Program
     {
+        //Declarando um delegate
+        delegate int Calcular(int a, int b);
+
         static void Main(string[] args)
         {
+
+            Calcular calSoma = new Calcular(Soma);
+            int soma = calSoma(50, 30);
+
+            Calcular calSubtracao = new Calcular(Subtracao);
+            int subtracao = calSubtracao(60, 25);
+            
+            
+            //Forma tradicional de chamar métodos
+            /*
+            var soma = Soma(10, 20);
+            var subtracao = Subtracao(20, 10);
+            */
+            Console.WriteLine("Soma: " + soma);
+
+            Console.WriteLine("Subtração: " + subtracao);
+
+            Console.ReadLine();
+        }
+
+        static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtracao (int a, int b)
+        {
+            return a - b;
         }
     }
 }
